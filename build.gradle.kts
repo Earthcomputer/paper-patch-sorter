edc9002b5a0b089aadf98547232d262429d668a6
Joe <burtonjae@hotmail.co.uk>
2022-08-20 10:22:54 +0100

    Add application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    application
 }
 
 group = "net.earthcomputer"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("net.earthcomputer.paperpatchsorter.Main")
+}
 
 repositories {
     mavenCentral()
